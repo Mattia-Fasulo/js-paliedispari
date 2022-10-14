33dbb40c9a9f9bdfe0490fbf4ba5a42debc504f9
Mattia-Fasulo <mattia.fasulo@gmail.com>
2022-10-14 17:47:22 +0200

    add controll to empty string

--- js/script.js
+++ js/script.js
@@ -4,17 +4,24 @@ const parolaHTML = document.getElementById('parola');
 const buttonHTML = document.querySelector('button');
 const contBtnHTML = document.getElementById('cont-btn')
 
-function isPalindromo(){
+function isPalindromo() {
     //ripulisco il div che ho creato per stampare il risultato
     const resultToRemove = document.getElementById('my-div');
-    if(resultToRemove) resultToRemove.remove();
-    
+    if (resultToRemove) resultToRemove.remove();
+
     //prendo la parola dell'utente e ne faccio il reverse
     let parolaUtente = parolaHTML.value;
     let parRev = reverseString(parolaUtente);
 
+
+    //controllo che non sia stata inserita una stringa vuota
+    if (parolaUtente == '') {
+        let result = createDivText('Devi inserire una parola!')
+        contBtnHTML.append(result);
+    }
+
     //controllo se la parola è palindroma e se lo è avviso l'utente
-    if( parolaUtente == parRev){
+    else if (parolaUtente == parRev) {
         let result = createDivText('La parola inserita è palindroma!')
         contBtnHTML.append(result);
     }
